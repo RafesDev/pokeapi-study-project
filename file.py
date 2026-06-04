8627df4b3d5cd3931f379f603cccfe868b4fb686
Rafes <rafes.dev5810@gmail.com>
2026-06-04 16:50:17 -0300

    Extract download progress message into function

--- file.py
+++ file.py
@@ -1,6 +1,17 @@
 import requests
 import json
 
+def downloading_message(pokemon_number, pokemon_total, pokemon, last_length):
+
+      message = f"[{pokemon_number}/{pokemon_total}] Getting data for {pokemon['name']}..."
+
+      padding = " " * max(0, last_length - len(message))
+
+      print(f"\r{message}{padding}", end="", flush=True)
+
+      return message
+
+
 pokemon_types_databank = {}
 
 response = requests.get(
@@ -17,33 +28,29 @@ running = True
 
 while running:
 
-            for pokemon in data['results']:
-                  
-                  pokemon_number += 1
+      for pokemon in data['results']:
+            
+            pokemon_number += 1
 
-                  message = f"[{pokemon_number}/{pokemon_total}] Getting data for {pokemon['name']}..."
+            message = downloading_message(pokemon_number, pokemon_total, pokemon, last_length)
 
-                  padding = " " * max(0, last_length - len(message))
+            last_length = len(message)
 
-                  print(f"\r{message}{padding}", end="", flush=True)
-
-                  last_length = len(message)
-
-                  response = requests.get(pokemon['url'])
-                  pokemon_data = response.json()
+            response = requests.get(pokemon['url'])
+            pokemon_data = response.json()
 
 
-                  types = []
-                  for type in pokemon_data['types']:
-                        types.append(type['type']['name'])
+            types = []
+            for type in pokemon_data['types']:
+                  types.append(type['type']['name'])
 
-                  pokemon_types_databank[pokemon['name']] = types
+            pokemon_types_databank[pokemon['name']] = types
 
-            if data['next'] != None:
-                  response = requests.get(data['next'])
-                  data = response.json()
-            else:
-                  running = False
+      if data['next'] != None:
+            response = requests.get(data['next'])
+            data = response.json()
+      else:
+            running = False
                   
 
 print()
